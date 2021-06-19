94148b305eec68d5f8eafb10d23abc968b49da38
Brett Fleming <brettfleming015@gmail.com>
2021-06-18 22:31:29 -0500

    minor changes

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,18 @@
 // Assignment Code\
 const passwordElement = document.getElementById('password')
+const generateElement = document.getElementById('generate')
+
+
+
+
+
+// password criteria elements
 const lengthElement = document.getElementById('length')
 const lowercaseElement = document.getElementById('lowercase')
 const uppercaseElement = document.getElementById('uppercase')
 const numbersElement = document.getElementById('numbers')
 const symbolsElement = document.getElementById('symbols')
-const generateElement = document.getElementById('generate')
+
 
 
 const randomfunction = {
@@ -15,40 +22,26 @@ const randomfunction = {
   symbol: randomSymbol
 };
 
-// Write password to the #password input
-generateElement.addEventListener("click", () => {
-  let length = lengthElement.value;
-  const hasLower = lowercaseElement.checked;
-  const hasUpper = uppercaseElement.checked;
-  const hasNumber = numbersElement.checked;
-  const hasSymbol = symbolsElement.checked;
-  
-  if (length < 8 || length > 128) {
-    length = prompt("please enter a valid length.")
-  };
-  
-    passwordElement.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length);
-});
 function generatePassword(lower, upper, number, symbol, length) {
   let generatedPassword = '';
 // need to count the types being selected
-  const typesCount = lower + upper + number + symbol;
+  const Count = lower + upper + number + symbol;
   // then we need to filter out the ones coming back as false
-  const typesArray = [ { lower }, { upper}, {number}, {symbol}].filter(item => Object.values(item)[0]);
+  const typesArr = [ {lower}, { upper}, {number}, {symbol}].filter(item => Object.values(item)[0]);
 
-if (typesCount === 0) {
+if (Count === 0) {
   return '';
 }
-  for (let i = 0; i < length; i += typesCount) {
-    typesArray.forEach(type => {
-      const functionName = Object.keys(type)[0];
+  for (let i = 0; i < length; i += Count) {
+    typesArr.forEach(type => {
+      const function1 = Object.keys(type)[0];
 
-      generatedPassword += randomfunction[functionName]();
+      generatedPassword += randomfunction[function1]();
     });
   }
   //if length isnt diviable by types counted evenly the password with be returned too long this line cuts it down to the length
-  var finalpassword = generatedPassword.slice(0, length); 
-  return finalpassword;
+  var password = generatedPassword.slice(0, length); 
+  return password;
 }
 
 
@@ -67,3 +60,18 @@ function randomSymbol() {
   const symbols = "!@#$%^&*(){}[]=<>/,.?"
   return symbols[Math.floor(Math.random() * symbols.length)]
 }
+
+
+generateElement.addEventListener("click", function() {
+  let length = lengthElement.value;
+  const lowerSelected = lowercaseElement.checked;
+  const upperSelected = uppercaseElement.checked;
+  const numberSelected = numbersElement.checked;
+  const symbolSelected = symbolsElement.checked;
+  
+  if (length < 8 || length > 128) {
+    length = prompt("please enter a valid length.")
+  };
+  
+    passwordElement.innerText = generatePassword(lowerSelected, upperSelected, numberSelected, symbolSelected, length);
+});
